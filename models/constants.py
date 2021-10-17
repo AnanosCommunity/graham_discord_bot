9497ad64084f50e31b26f509e7b90059846a3fa5
Not Thomiz <jeanouina@lightcord.org>
2021-10-17 22:29:36 +0200

    rain requirements

--- models/constants.py
+++ models/constants.py
@@ -5,6 +5,6 @@ class Constants(object):
     TIPRANDOM_MINIMUM = 5.0
     TIP_UNIT = 'ANA'
     WITHDRAW_COOLDOWN = 1 # Seconds
-    RAIN_MIN_ACTIVE_COUNT = 5 # Amount of people who have to be active for rain to work
-    RAIN_MSG_REQUIREMENT = 5 # Amount of decent messages required to receive rain
+    RAIN_MIN_ACTIVE_COUNT = 2 # Amount of people who have to be active for rain to work
+    RAIN_MSG_REQUIREMENT = 3 # Amount of decent messages required to receive rain
     REPRESENTATIVE='ana_38enhxt6k5izyrc47tptcdytga7uhftworydqsbm7gsfgecmrjrowou349ae'
